1926f7943e714c51f00e211065b3833f2647b08f
LSO107 <34274877+LSO107@users.noreply.github.com>
2019-04-15 23:53:36 +0100

    House Destruction & Lower Building Adjustment
    * House Destruction (Walking over them)
    - includes points, explosion and rubble
    * Removed mesh collider from Low Buildings as they had two, these are expensive and were not needed,
    - replaced with a single box collider, which is higher so we can always punch them (was a bit of a struggle to hit them before).
    * New asset pack
    * Removed lots of asset that aren't being used
    * Removed components from prefabs that aren't being used (mostly colliders & rigidbodies)

---
+++ Assets/Scripts/Objects/Destructible/Objects/House.cs
@@ -0,0 +1,36 @@
+﻿using Objects.Destructible.Definition;
+using UnityEngine;
+
+namespace Objects.Destructible.Objects
+{
+    internal sealed class House : DestructibleObject
+    {
+        [SerializeField] private GameObject destroyedHouse;
+        [SerializeField] private GameObject particlePosition;
+        [SerializeField] private GameObject particleEffect;
+
+        // Instantiate particle effect and enable/disable building + destruction
+        // - Don't like this way, but the models transforms are all messed up
+        // and it'll be too time consuming to fix it all
+        //
+        public override void Destruct()
+        {
+            Instantiate(particleEffect, particlePosition.transform.position, Quaternion.identity);
+            gameObject.SetActive(false);
+            destroyedHouse.gameObject.SetActive(true);
+        }
+
+        // Check the player is colliding, then call destruct
+        // and award the points for destruction
+        //
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                Destruct();
+
+                ScoreManager.AddScore(scoreAwarded);
+            }
+        }
+    }
+}
