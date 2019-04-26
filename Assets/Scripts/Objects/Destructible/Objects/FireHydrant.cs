d06f142de227693db0017b05f8dc3025ddc4caff
LSO107 <34274877+LSO107@users.noreply.github.com>
2019-04-26 02:12:18 +0100

    Tweaks
    fire hydrant script
    lamp post collider
    road tiles use box collider instead of mesh

--- Assets/Scripts/Objects/Destructible/Objects/FireHydrant.cs
+++ Assets/Scripts/Objects/Destructible/Objects/FireHydrant.cs
@@ -1,6 +1,4 @@
-﻿using System.Security.Cryptography;
-using Traffic_System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Objects.Destructible.Objects
 {
@@ -9,23 +7,18 @@ namespace Objects.Destructible.Objects
         [SerializeField]
         private ParticleSystem particleEffect;
 
-        private bool m_Knocked;
-
-        private void OnCollisionEnter(Collision other)
+        private void OnTriggerEnter(Collider other)
         {
             if (!other.gameObject.CompareTag("Player"))
                 return;
 
-            if (m_Knocked)
-            {
-                Physics.IgnoreCollision(other.gameObject.GetComponent<Collider>(), gameObject.GetComponent<Collider>());
-            }
-            else
-            {
-                m_Knocked = true;
-                Instantiate(particleEffect, transform.position, Quaternion.Euler(-90, 0, 0));
-                Physics.IgnoreCollision(other.gameObject.GetComponent<Collider>(), gameObject.GetComponent<Collider>());
-            }
+            var rb = gameObject.GetComponent<Rigidbody>();
+            rb.isKinematic = false;
+
+            Instantiate(particleEffect, transform.position, Quaternion.Euler(-90, 0, 0));
+
+            Destroy(gameObject.GetComponent<Collider>(), 2);
+            Destroy(this);
         }
     }
 }
