e87bebb0160525a37bedcb18d77fe976b13dfd75
LSO107 <34274877+LSO107@users.noreply.github.com>
2019-04-26 17:06:14 +0100

    First walking route & human added
    First walking route working
    Human that can be squashed (blood splatter) and gives points

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+internal sealed class Human : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bloodSplatter;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Player"))
+            return;
+
+        Instantiate(bloodSplatter, transform.position, Quaternion.Euler(0,0,90));
+        ScoreManager.AddScore(10);
+        Destroy(gameObject);
+    }
+}
