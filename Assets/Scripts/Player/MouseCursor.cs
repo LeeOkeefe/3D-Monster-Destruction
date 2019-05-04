6b0667f7f8819b4a7ccff0e4af99baf1b2e8cdd4
LSO107 <34274877+LSO107@users.noreply.github.com>
2019-05-04 17:34:29 +0100

    Mouse cursor hiding

---
+++ Assets/Scripts/Player/MouseCursor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour {
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void ToggleMouse(bool show)
+    {
+        Cursor.lockState = show
+            ? CursorLockMode.None
+            : CursorLockMode.Locked;
+
+        Cursor.visible = show;
+    }
+}
